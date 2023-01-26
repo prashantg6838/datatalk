14d1e470a70caf9ab9aa3af5964f6eba722c6e85
Prashant <pgdreams6838@gmail.com>
2023-01-26 16:17:50 +0530

    first commit

---
+++ pipeline.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+# Feel free to write Pandas code here
+# But the import alone makes sure Pandas is being installed properly
+print(sys.argv)
+
+day = sys.argv[1]
+
+print(f"job finished successfully for for day = {day}")
