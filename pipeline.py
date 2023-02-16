309e56e972514eec6325acb61843433b97164ab0
prashant_gunjeti <122257004+prashantg6838@users.noreply.github.com>
2023-02-16 09:50:34 +0530

    delete

--- pipeline.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import sys
-# Feel free to write Pandas code here
-# But the import alone makes sure Pandas is being installed properly
-print(sys.argv)
-
-day = sys.argv[1]
-
-print(f"job finished successfully for for day = {day}")
